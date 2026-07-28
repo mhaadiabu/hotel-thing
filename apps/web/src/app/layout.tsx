8bfd25f934fb5dadef0c0a8c5ee99dd56d93d7c5
mhaadiabu <mhabubakar999@outlook.com>
2026-07-28 07:58:03 +0000

    apply preset b22mgn5jhy

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,9 +3,14 @@ import { shadcn } from "@clerk/ui/themes";
 import type { Metadata } from "next";
 
 import "../index.css";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Nunito_Sans, Raleway } from "next/font/google";
 
 import Providers from "@/components/providers";
+import { cn } from "@hotel/ui/lib/utils";
+
+const ralewayHeading = Raleway({subsets:['latin'],variable:'--font-heading'});
+
+const nunitoSans = Nunito_Sans({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", nunitoSans.variable, ralewayHeading.variable)}>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClerkProvider appearance={{ theme: shadcn }}>
           <Providers>
